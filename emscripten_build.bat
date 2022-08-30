bc4dd8608984ab2bcbea999660358edcf7817773
dandistine <34634876+dandistine@users.noreply.github.com>
2022-08-30 18:01:54 -0500

    Sound part 2

--- emscripten_build.bat
+++ emscripten_build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 call C:\Users\dandistine\emsdk\emsdk_env.bat
-call em++ -std=c++17 -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_LIBPNG=1 -s SINGLE_FILE --shell-file basic_template.html VenusSigil/main.cpp -I VenusSigil -o venus_sigil.html
+call em++ -std=c++17 -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_SDL_MIXER=2 -s USE_LIBPNG=1 -s SINGLE_FILE --shell-file basic_template.html VenusSigil/main.cpp -I VenusSigil -o venus_sigil.html
